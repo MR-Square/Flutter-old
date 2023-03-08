ffc8355d5b2daab89328566d8a56ffd655cd413c
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-03-08 23:35:06 +0530

    day 1 dart 20%

---
+++ day_1.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  print('Hello world!');
+  // it is used to print the output on the output screen.
+
+  // Variables
+
+  // declaring a variable
+  int a = 10;
+  var b = 20; // this is called type inference.
+  print(a);
+  print(b);
+
+  double pi = 3.14;
+  print(pi);
+
+  String name = "mohd raza";
+  print(name);
+
+  bool isIn = true;
+  print(isIn);
+
+  // Arithmetic Operator
+  /*
+  +     |   addition        | add two numbers
+  -     |   subtraction     | subtracts two numbers
+  *     |   multiplication  | multiplly two numbers
+  /     |   division        | divides two numbers and gives quotient
+  %     |   modulus         | divides two numbers and gives remainder
+  ++x   |   pre-increment   | increments x by  1 and returns it
+  x++   |   post-increment  | returns the original and then increments by 1
+  --x   |   pre-decrement   | decrements x by  1 and returns it
+  x--   |   post-decrement  | returns the original and then decrements by 1
+  
+  */
+}

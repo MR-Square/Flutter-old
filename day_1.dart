0c3e79de7bffa423b9d8761b675343e38aec12aa
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-03-09 00:33:03 +0530

    day one completed

--- day_1.dart
+++ day_1.dart
@@ -1,3 +1,5 @@
+import 'dart:io'; // importing so that we can use "stdin"
+
 void main(List<String> args) {
   print('Hello world!');
   // it is used to print the output on the output screen.
@@ -9,14 +11,11 @@ void main(List<String> args) {
   var b = 20; // this is called type inference.
   print(a);
   print(b);
-
   double pi = 3.14;
-  print(pi);
-
   String name = "mohd raza";
-  print(name);
-
   bool isIn = true;
+  print("value of pi = $pi");
+  print("my name is $name");
   print(isIn);
 
   // Arithmetic Operator
@@ -30,6 +29,64 @@ void main(List<String> args) {
   x++   |   post-increment  | returns the original and then increments by 1
   --x   |   pre-decrement   | decrements x by  1 and returns it
   x--   |   post-decrement  | returns the original and then decrements by 1
-  
+  >     |   greater than    | check whethere LHS is greater or not
+  <     |   less than       | check whethere LHS is smaller or not
+  >=    |   great/= to      | check whethere LHS is "greater or equal to" or not
+  <=    |   lesser/= to     | check whethere LHS is "lesser or equal to" or not
+  !=    |   not equal to    | check whethere both the sides are equal or not
+  &&    |   AND             | true only if both operands are true
+  ||    |   OR              | true if either ooperand is true
+  !     |   NOT             | changes true to false and false to true
   */
+
+  // Type casting
+  print("\nType casting");
+  var div1 = 4 / 6;
+  print(div1);
+  print(div1.toInt());
+
+  var s = "1";
+  int.parse(s);
+  print(s);
+
+  // Conditional statements
+  print("\n Conditional statement");
+  int age = 20;
+  if (age > 18) {
+    print("you ae eligible");
+  } else {
+    print("you are not eligible");
+  }
+
+  // Taking input from user.
+  var day = stdin.readLineSync();
+
+  // Switch case
+  print("\n Switch case");
+
+  switch (day) {
+    case "Monday":
+      print("it is monday");
+      break;
+    case "Tuesday":
+      print("it is tuesday");
+      break;
+    case "Wednesday":
+      print("it is wednesday");
+      break;
+    default:
+      print("enter correct day name");
+  }
+
+  // Ternary operator
+  /*
+  Syntax: condition?expression1:expression2
+
+  if condition is true, expression1 will execute
+  if condition is false, expression2 will execute
+  */
+  var no1 = 20;
+  var no2 = 30;
+  var mymax = no1 > no2 ? no1 : no2;
+  print("my max value is $mymax");
 }

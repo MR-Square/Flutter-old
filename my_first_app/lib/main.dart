6a9a8dc3d0fb6c4c268988457b24e99ec7e73ed1
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-03-11 01:16:07 +0530

    day 4 45% completed

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -4,14 +4,21 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // state variable
+  String mytext = "My first App";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My First App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,22 +33,43 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(),
-        body: Row(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.star),
-            Center(
-              child: Text(
-                "My first app",
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Color.fromARGB(255, 223, 95, 56),
-                  letterSpacing: 5,
-                  backgroundColor: Colors.yellow,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.star),
+                Center(
+                  child: Text(
+                    mytext,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      color: Color.fromARGB(255, 223, 95, 56),
+                      letterSpacing: 5,
+                      backgroundColor: Color.fromARGB(255, 134, 228, 241),
+                    ),
+                  ),
                 ),
-              ),
+                const Icon(Icons.star),
+              ],
             ),
-            Icon(Icons.star),
+            OutlinedButton(
+              onPressed: () {
+                mytext = "Flutter is best";
+                // print("You haved cliked the button");
+                setState(() {});
+              },
+              child: const Text("click me"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  mytext = "My First App";
+                });
+              },
+              child: const Text("click me"),
+            ),
           ],
         ),
       ),

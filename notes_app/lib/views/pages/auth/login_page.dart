81f1d48bf95741082641146bbeef2326836fa970
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-03-16 20:24:03 +0530

    login page completed

--- notes_app/lib/views/pages/auth/login_page.dart
+++ notes_app/lib/views/pages/auth/login_page.dart
@@ -8,23 +8,64 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isObscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text("Login"),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Login",
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Password",
+            Column(
+              children: [
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+                TextField(
+                  obscureText: isObscureText,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        isObscureText = !isObscureText;
+                        setState(() {});
+                      },
+                      icon: Icon(isObscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Login"),
+            ),
+            Column(
+              children: [
+                const Text("Do not have an account?"),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("sign up"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
